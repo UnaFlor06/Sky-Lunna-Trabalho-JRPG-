ae34250fa7938833fc070f1e306a3c05117bb2af
Hootsuki <hbiancchi@gmail.com>
2025-10-06 20:50:22 -0300

    giraaasss

--- src/Inimigos.java
+++ src/Inimigos.java
@@ -1,8 +1,8 @@
 public class Inimigos {
     private String ReiSlime;
-    (vida50 dano5);
+    //(vida50 dano5);
     private String GoblinAnciaon;
-    (vida75 dano 15);
+    //(vida75 dano 15);
     private String NecromanteAntigo;
-    (vida100 dano25);
+    //(vida100 dano25);
 }
