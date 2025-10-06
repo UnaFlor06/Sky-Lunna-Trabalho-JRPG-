0eb7cf446b83280874695d4555fc5b0f0eef925e
Hootsuki <hbiancchi@gmail.com>
2025-10-06 14:33:56 -0300

    tentei

--- src/Combat.java
+++ src/Combat.java
@@ -1,3 +1,6 @@
 public class Combat {
-    
+    private int calcularDano(int ataque, int defesa) {
+        int dano = ataque - defesa;
+        return Math.max(1, dano); // Garante que o dano mínimo é 1
+    }
 }
