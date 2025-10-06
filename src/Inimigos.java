5a7100bb6d29995a3052bebf2ad08a21c0812988
Hootsuki <hbiancchi@gmail.com>
2025-10-06 20:46:59 -0300

    gui gui

---
+++ src/Inimigos.java
@@ -0,0 +1,8 @@
+public class Inimigos {
+    private String ReiSlime;
+    (vida50 dano5);
+    private String GoblinAnciaon;
+    (vida75 dano 15);
+    private String NecromanteAntigo;
+    (vida100 dano25);
+}
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,6 +1,8 @@
 public class Weapon {
     private String Cajado;
+    (cnst 5 minimo);
     private String Arco;
+    (dext 5 minimo);
     private String Foice;
-    
+    (dext 5 + str 5 minimo);
 }
